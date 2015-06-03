782f3fe80a438ee8a1be67f4c1d17dbca9f7d636
Bryan Finlayson <bdfinlayson@gmail.com>
2015-06-03 18:39:25 -0500

    Add good basic tests for signing up users.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,11 +2,36 @@ require 'spec_helper'
 require_relative '../support/utilities'
 
 describe "User pages" do
-    subject { page }
-    describe 'signup page' do
-      before { visit signup_path }
+  subject { page }
+  describe 'signup page' do
+    before { visit signup_path }
     it { should have_content('Sign up') }
     it { should have_title(full_title('Sign up')) }
+  end
+
+  describe 'signup' do 
+    before { visit signup_path }
+
+    let(:submit) { 'Create my account' }
+
+    describe 'with invalid information' do
+      it 'should not create a user' do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+
+      describe 'with valid information' do
+        before do
+          fill_in 'Name', with: 'Example User'
+          fill_in 'Email', with: 'user@example.com'
+          fill_in 'Password', 'foobar'
+          fill_in 'Confirmation', 'foobar'
+        end
+
+        it 'should create a user' do
+          expect { click_button submit }.to change(User, :count).by(1)
+        end
+      end
+    end
   end
 
   describe 'profile page' do
