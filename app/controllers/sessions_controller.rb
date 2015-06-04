6589765d93884906493dccc9e0304a8c9384d9bb
Bryan Finlayson <bdfinlayson@gmail.com>
2015-06-04 13:30:09 -0500

    Fix typo in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(paras[:session][:password])
+    if user && user.authenticate(params[:session][:password])
       # Sign the user in and redirect to the user's show page
     else
       flash[:error] = 'Invalid email/password combination' # not quite right!
