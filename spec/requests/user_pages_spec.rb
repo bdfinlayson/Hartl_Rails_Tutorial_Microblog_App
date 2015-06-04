d552ba69adb5eebf2108580b09d73a2218c666c6
Bryan Finlayson <bdfinlayson@gmail.com>
2015-06-03 21:04:24 -0500

    Using strong parameters in the create action. Fixing missing 'with' in user pages spec test

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -23,8 +23,8 @@ describe "User pages" do
         before do
           fill_in 'Name', with: 'Example User'
           fill_in 'Email', with: 'user@example.com'
-          fill_in 'Password', 'foobar'
-          fill_in 'Confirmation', 'foobar'
+          fill_in 'Password', with: 'foobar'
+          fill_in 'Confirmation', with: 'foobar'
         end
 
         it 'should create a user' do
