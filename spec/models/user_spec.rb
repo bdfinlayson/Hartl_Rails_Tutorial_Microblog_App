fb20de115d587213421507e1c66af202512db335
Bryan Finlayson <bdfinlayson@gmail.com>
2015-06-04 14:34:14 -0500

    A test for a valid (nonblank) remember token.
    the its method, which is like it but applies the subsequent test to the
    given attribute rather than the subject of the test. In other words,
    
        its(:remember_token) { should_not be_blank }
    
    is equivalent to:
    
        it { expect(@user.remember_token).not_to be_blank }

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -106,5 +106,10 @@ describe User do
       @user.save
       expect(@user.reload.email).to eq mixed_case_email.downcase
     end
+
+    describe 'remember token' do
+      before { @user.save }
+      its(:remember_token) { should_not be_blank }
+    end
   end
 end
