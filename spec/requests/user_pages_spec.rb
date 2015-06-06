0708630b6afffd5e3a184c6071860e9e34ca6d9a
Bryan Finlayson <bdfinlayson@gmail.com>
2015-06-06 15:52:22 -0500

    Add tests for the user edit page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -48,6 +48,23 @@ describe "User pages" do
           it { should have_selector( 'div.alert.alert-success', text: 'Welcome to the Sample App!') }
           it { should have_content('Example User') }
           it { should have_link('Sign out') }
+
+          describe 'edit' do
+            let(:user) { FactoryGirl.create(:user) }
+            before { visit edit_user_path(user) }
+
+            describe 'page' do
+              it { should have_content('Update your profile') }
+              it { should have_title('Edit user') }
+              it { should have_link('change', href: 'http://gravatar.com/emails') }
+            end
+
+            describe 'with invalid information' do
+              before { click_button 'Save changes' }
+
+              it { should have_content('error') }
+            end
+          end
         end
       end
     end
