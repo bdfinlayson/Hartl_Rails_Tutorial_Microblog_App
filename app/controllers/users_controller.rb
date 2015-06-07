30f922217f3c2601999404b0e9a1d9341b123338
Bryan Finlayson <bdfinlayson@gmail.com>
2015-06-07 12:05:09 -0500

    Add delete action for admin users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ require 'pry'
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
@@ -28,6 +29,12 @@ class UsersController < ApplicationController
   end
 
   def edit
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = 'User deleted.'
+    redirect_to users_url
   end
 
   def update
@@ -57,5 +64,9 @@ class UsersController < ApplicationController
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 end
