0e2e9b4fe855a9b185a270da7b2f159ccf0854e9
Bryan Finlayson <bdfinlayson@gmail.com>
2015-06-11 16:50:36 -0500

    Improving the status feed from_users_followed_by

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,8 +4,11 @@ class Micropost < ActiveRecord::Base
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 
+  # Returns microposts from the users being followed by the given user.
   def self.from_users_followed_by(user)
-    followed_user_ids = user.followed_user_ids
-    where('user_id in (?) or user_id = ?', followed_user_ids, user)
+    followed_user_ids = 'select followed_id from relationships
+                        where follower_id = :user_id'
+    where("user_id in (#{followed_user_ids}) or user_id = :user_id",
+          user_id: user)
   end
 end
